c22be4f5e92cef94a3a3676516ae62d4c17e2d9d
Thuong Nguyen <thuongnv@hp-backup.com>
2021-10-13 01:48:24 +0700

    initial data

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,14 +1,17 @@
-import { Task } from 'components/Task/Task'
+import { Card } from 'components/Card/Card'
 import React from 'react'
+import { mapOther } from 'untilies/sorts'
 
 import './Column.scss'
 
-export const Column = () => {
+export const Column = (props) => {
+    const { column } = props
+    const cards  = mapOther(column.cards,column.CardOther,'id')
     return (
         <div className="column">
-        <header>brain store</header>
-        <ul className="task-list">
-            <Task/>
+        <header>{column.title}</header>
+        <ul className="card-list">
+            {cards.map((card,index)=> <Card key={index} card={card}/>)}
         </ul>
         <footer>Add another card</footer>
       </div>
